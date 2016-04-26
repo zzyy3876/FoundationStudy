ddf8f88b01a3aadba43e7cecb6b2a131fddff9a5
AlstonZhao <zzyy3876@live.com>
2016-04-25 17:30:54 -0700

    commit by Zhao

--- src/SortArray/MergeSort.java
+++ src/SortArray/MergeSort.java
@@ -11,7 +11,6 @@ public class MergeSort {
         //  merge(array);
         for(int i = 0; i < array.length; i++){
             System.out.println(array[i]);
-            System.out.println(i);
         }
     }
 }
