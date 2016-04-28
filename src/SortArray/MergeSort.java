59b4a44179e306f2ece72aea49d73aeeaabba8a6
Alston <zzyy3876@live.com>
2016-04-27 18:25:45 -0700

    by Alston

--- src/SortArray/MergeSort.java
+++ src/SortArray/MergeSort.java
@@ -7,7 +7,7 @@ package SortArray;
 
 public class MergeSort {
     public static void main(String args[]){
-        int[] array = {10, 179, 9, 1, 12, 7, 3};
+        int[] array = {10, 190, 9, 1, 12, 7, 3};
         //  merge(array);
         for(int i = 0; i < array.length; i++){
             System.out.println(array[i]);
